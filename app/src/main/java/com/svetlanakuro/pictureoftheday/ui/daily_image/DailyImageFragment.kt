7b510eb3a1460e99f2c2be7dc902989dd51de47c
Svetlana Kuro <svetlana.kuro@gmail.com>
2022-02-24 06:20:00 +0300

    BottomSheet is replaced to CollapsingToolbar.

--- app/src/main/java/com/svetlanakuro/pictureoftheday/ui/daily_image/DailyImageFragment.kt
+++ app/src/main/java/com/svetlanakuro/pictureoftheday/ui/daily_image/DailyImageFragment.kt
@@ -2,10 +2,8 @@ package com.svetlanakuro.pictureoftheday.ui.daily_image
 
 import android.os.Bundle
 import android.view.*
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.*
 import com.bumptech.glide.Glide
-import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.svetlanakuro.pictureoftheday.R
 import com.svetlanakuro.pictureoftheday.data.entity.DailyImageResponse
 import com.svetlanakuro.pictureoftheday.databinding.FragmentDailyImageBinding
@@ -17,8 +15,6 @@ class DailyImageFragment : Fragment(R.layout.fragment_daily_image) {
 
     private val viewModel by viewModels<DailyImageViewModel>()
 
-    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
@@ -28,7 +24,6 @@ class DailyImageFragment : Fragment(R.layout.fragment_daily_image) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initViews()
         initViewModel()
         viewModel.onViewIsReady(requireActivity().application)
     }
@@ -37,35 +32,13 @@ class DailyImageFragment : Fragment(R.layout.fragment_daily_image) {
         viewModel.renderImageDataLiveData.observe(viewLifecycleOwner) { dailyImage ->
             renderImageData(dailyImage)
         }
-        viewModel.bottomSheetStateLiveData.observe(viewLifecycleOwner) { currentState ->
-            bottomSheetBehavior.state = currentState
-        }
-    }
-
-    private fun initViews() {
-        initBottomSheet()
-    }
-
-    private fun initBottomSheet() {
-        bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheet.bottomSheetContainer)
-        val bottomSheetCallback = object : BottomSheetBehavior.BottomSheetCallback() {
-            override fun onStateChanged(bottomSheet: View, newState: Int) {
-                viewModel.onBottomSheetStateChanged(newState)
-            }
-
-            override fun onSlide(bottomSheet: View, slideOffset: Float) {
-                //do nothing
-            }
-        }
-        bottomSheetBehavior.addBottomSheetCallback(bottomSheetCallback)
     }
 
     private fun renderImageData(dailyImage: DailyImageResponse) {
         val imageUrl = dailyImage.url
         Glide.with(requireContext()).load(imageUrl).into(binding.dailyImageView)
-        binding.bottomSheet.titleTextView.text = dailyImage.title
-        binding.bottomSheet.copyrightTextView.text = dailyImage.copyright
-        binding.bottomSheet.imageDescriptionTextView.text = dailyImage.explanation
+        binding.mainToolbar.title = dailyImage.title
+        binding.imageDescriptionTextView.text = dailyImage.explanation
     }
 
     override fun onDestroyView() {
