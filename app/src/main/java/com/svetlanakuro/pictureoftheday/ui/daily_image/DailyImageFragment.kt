fa3d322f34399cd07775ba04afe259ccf65da01e
Svetlana Kuro <svetlana.kuro@gmail.com>
2022-03-14 19:05:40 +0300

    Added animation on the main screen, on DailyImageFragment.kt.

--- app/src/main/java/com/svetlanakuro/pictureoftheday/ui/daily_image/DailyImageFragment.kt
+++ app/src/main/java/com/svetlanakuro/pictureoftheday/ui/daily_image/DailyImageFragment.kt
@@ -1,24 +1,29 @@
 package com.svetlanakuro.pictureoftheday.ui.daily_image
 
 import android.os.Bundle
+import android.transition.*
 import android.view.*
+import android.view.animation.AnticipateOvershootInterpolator
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.fragment.app.*
 import com.bumptech.glide.Glide
 import com.svetlanakuro.pictureoftheday.R
 import com.svetlanakuro.pictureoftheday.data.entity.DailyImageResponse
-import com.svetlanakuro.pictureoftheday.databinding.FragmentDailyImageBinding
+import com.svetlanakuro.pictureoftheday.databinding.FragmentDailyImageStartBinding
 
-class DailyImageFragment : Fragment(R.layout.fragment_daily_image) {
+class DailyImageFragment : Fragment(R.layout.fragment_daily_image_start) {
 
-    private var _binding: FragmentDailyImageBinding? = null
+    private var _binding: FragmentDailyImageStartBinding? = null
     private val binding get() = _binding!!
 
     private val viewModel by viewModels<DailyImageViewModel>()
 
+    private var show = false
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentDailyImageBinding.inflate(inflater, container, false)
+        _binding = FragmentDailyImageStartBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -26,6 +31,7 @@ class DailyImageFragment : Fragment(R.layout.fragment_daily_image) {
         super.onViewCreated(view, savedInstanceState)
         initViewModel()
         viewModel.onViewIsReady(requireActivity().application)
+        binding.constraintContainer.setOnClickListener { if (show) hideComponents() else showComponents() }
     }
 
     private fun initViewModel() {
@@ -37,8 +43,36 @@ class DailyImageFragment : Fragment(R.layout.fragment_daily_image) {
     private fun renderImageData(dailyImage: DailyImageResponse) {
         val imageUrl = dailyImage.url
         Glide.with(requireContext()).load(imageUrl).into(binding.dailyImageView)
-        binding.mainToolbar.title = dailyImage.title
+        binding.title.text = dailyImage.title
         binding.imageDescriptionTextView.text = dailyImage.explanation
+    }
+
+    private fun showComponents() {
+        show = true
+
+        val constraintSet = ConstraintSet()
+        constraintSet.clone(this@DailyImageFragment.context, R.layout.fragment_daily_image_end)
+
+        val transition = ChangeBounds()
+        transition.interpolator = AnticipateOvershootInterpolator(1.0f)
+        transition.duration = 1200
+
+        TransitionManager.beginDelayedTransition(binding.constraintContainer, transition)
+        constraintSet.applyTo(binding.constraintContainer)
+    }
+
+    private fun hideComponents() {
+        show = false
+
+        val constraintSet = ConstraintSet()
+        constraintSet.clone(this@DailyImageFragment.context, R.layout.fragment_daily_image_start)
+
+        val transition = ChangeBounds()
+        transition.interpolator = AnticipateOvershootInterpolator(1.0f)
+        transition.duration = 1200
+
+        TransitionManager.beginDelayedTransition(binding.constraintContainer, transition)
+        constraintSet.applyTo(binding.constraintContainer)
     }
 
     override fun onDestroyView() {
